7088ed79d4f2f47eede68b7fe9eb3aabcde4fca8
Radu Zamfir <zsradu@yahoo.com>
2025-03-15 16:39:49 +0000

    fix: update workshops schedule and speakers info

--- src/components/pages/speakers/speakers/speakers_config.jsx
+++ src/components/pages/speakers/speakers/speakers_config.jsx
@@ -1,9 +1,9 @@
 import AbdelSghiouarPhoto from './images/abdel-sghiouar.jpg';
 import AndreiPetrescuPhoto from './images/andrei-petrescu-photo.jpg';
+import ClaudiuSonelPhoto from './images/claudiu-sonel.jpg';
 import MarkSagiKazarPhoto from './images/mark-sagi-kazar-photo.jpg';
 import PatrickDeKruijfPhoto from './images/patrick-de-kruijf.jpg';
 import RaduDobrinescuPhoto from './images/radu-dobrinescu.png';
-import RaduVunvuleaPhoto from './images/radu-vunvulea.jpg';
 import RazvanValceanuPhoto from './images/razvan-valceanu.jpg';
 import StefanProdanPhoto from './images/stefan-prodan-photo.jpeg';
 import ViorelAnghelPhoto from './images/viorel-anghel.jpg';
@@ -35,6 +35,7 @@ const SPEAKERS = [
     position: 'DevOps Department Lead @ CloudHero',
     content:
       'Andrei Petrescu leads the DevOps Department at CloudHero and has experience with running Kubernetes in production for 7+ years.' +
+      '<br/><br/>' +
       'He has held workshops and presentations on various topics at Cloud Native Bucharest, Voxxed Days and CloudHero-organized events. He is a CKA and Professional Architect on AWS.',
     photo: AndreiPetrescuPhoto,
     twitterUrl: '',
@@ -53,6 +54,7 @@ const SPEAKERS = [
     position: 'Senior Platform Engineer at Metaminds',
     content:
       'With 20 years of experience in the tech trenches, I’ve seen it all—from bare-metal servers that needed a hug to cloud-native architectures that scale like magic.' +
+      '<br/><br/>' +
       'These days, I’m all about Kubernetes and cloud-native technologies, where my passion lies somewhere between “obsession” and “true love.” If it runs in a container, orchestrates with Kubernetes I’m probably already tinkering with it.',
     photo: VladMocanuPhoto,
     twitterUrl: '',
@@ -101,9 +103,9 @@ const SPEAKERS = [
   {
     id: '6',
     name: 'Márk Sági-Kazár',
-    position: 'Head of OSS @ OpenMeter',
+    position: 'CNCF Ambassador',
     content:
-      'Mark is a software engineer from Hungary passionate about open source both as a maintainer and as a contributor. Besides writing code, he loves every part of software engineering from software design to devops and infrastructure.' +
+      'Mark is a software engineer from Hungary passionate about open source both as a maintainer and as a contributor. Besides writing code, he loves every part of software engineering from software design to dev-ops and infrastructure.' +
       '<br/><br/>' +
       'When he is not coding, Mark attends a local folk dance group, organizes and attends meetups or just stays at home with a beer and a great book.',
     photo: MarkSagiKazarPhoto,
@@ -112,7 +114,7 @@ const SPEAKERS = [
     githubUrl: '',
     communityUrl: 'https://sagikazarmark.hu',
     instagramUrl: '',
-    websiteUrl: 'https://openmeter.io',
+    websiteUrl: '',
     time: '11:30',
     title: 'Workshop: Accelerate Your Development and CI/CD Pipelines with Dagger',
     duration: '90 min',
@@ -123,7 +125,9 @@ const SPEAKERS = [
     position: 'Azure Architect / Global Landing Zone Solutions Lead @ Xebia',
     content:
       'Patrick is a passionate consultant with broad knowledge of infrastructure and security aspects of IT. As a Cloud Architect and Specialist, he helps customers by designing, implementing, and assessing cloud architectures and solutions in alignment with his customers’ business and IT objectives.' +
+      '<br/><br/>' +
       'Throughout the past 16 years, Patrick has accumulated a broad experience in Infrastructure, Security, and Consulting, mainly at companies that relied heavily on Microsoft technology.' +
+      '<br/><br/>' +
       'He applies state-of-the-art technologies and concepts, including the cloud and DevOps, to help his customers create business value. His approach is one of honesty and building relationships, which always makes him a trusted advisor for his stakeholders. Patrick has a clear and analytical mindset and is a consultant with excellent communication and presentation skills.',
     photo: PatrickDeKruijfPhoto,
     twitterUrl: '',
@@ -142,7 +146,9 @@ const SPEAKERS = [
     position: 'Amazon Web Services, Sr. Partner Solutions Architect',
     content:
       'Radu Dobrinescu is a Senior Partner Solutions Architect at Amazon Web Services (AWS). He has been working in technology for 17 years spanning different technologies and roles.' +
+      '<br/><br/>' +
       'He is currently focused on building the AWS partner ecosystem across Central and Eastern Europe by helping AWS partners build their cloud center of excellence and deliver world-class cloud transformation journeys to their customers.' +
+      '<br/><br/>' +
       'Radu helps partners design and deploy innovative products and services that leverage the benefits of the AWS platform with a special focus on containerized workloads and DevOps practices and tools, as well as Data Analytics.',
     photo: RaduDobrinescuPhoto,
     twitterUrl: '',
@@ -162,6 +168,7 @@ const SPEAKERS = [
     position: 'Flux project Core Maintainer at ControlPlane',
     content:
       'Stefan is a Principal Engineer at ControlPlane and an open source contributor to cloud-native projects.' +
+      '<br/><br/>' +
       "He is the creator of Flagger the progressive delivery operator for Kubernetes, and a core maintainer of the CNCF's Flux project." +
       'Stefan has over 20 years of experience with software development and he enjoys programming in Go and writing about distributed systems.',
     photo: StefanProdanPhoto,
@@ -177,23 +184,21 @@ const SPEAKERS = [
   },
   {
     id: '10',
-    name: 'Radu Vunvulea',
-    position:
-      'Technology Enthusiast, Microsoft Azure MVP, Microsoft Regional Director, Group Head of Cloud - Endava',
+    name: 'Claudiu Sonel',
+    position: 'Senior DevOps Consultant @ ENDAVA',
     content:
-      'Radu Vunvulea is a technology enthusiast working as VP of Cloud for Endava.' +
-      'He has a vast experience in different technologies and industries. Most of his time is spent working with the cloud, helping companies to innovate and finding solutions to their business problems.' +
-      'He enjoys building bridges between people and helping others to grow. He shares his knowledge on his personal blog and at different events where he is invited as a speaker. In his spare time, he drives an IT community and he is also a Microsoft Regional Director and Microsoft Azure MVP.',
-    photo: RaduVunvuleaPhoto,
-    twitterUrl: 'https://twitter.com/RaduVunvulea',
-    linkedInUrl: 'https://linkedin.com/in/raduvunvulea/',
+      'Claudiu is an experienced Senior Engineer with a demonstrated history of working in the information technology and services industry. Skilled in Linux System Administration, AWS, GCP, Jenkins, Docker, Kubernetes and Infrastructure as Code. Claudiu has successfully designed and managed complex cloud-native infrastructures for diverse organizations.' +
+      '<br/><br/>' +
+      'As a passionate advocate for automation and scalability, Claudiu specializes in leveraging Kubernetes to create resilient, cost-effective , and high-performing systems. Claudiu empowers teams to unlock the full potential of AWS and Kubernetes for modern workloads.',
+    photo: ClaudiuSonelPhoto,
+    linkedInUrl: 'https://linkedin.com/in/claudiusonel/',
     githubUrl: '',
     communityUrl: '',
     instagramUrl: '',
-    websiteUrl: 'https://vunvulearadu.blogspot.com',
+    websiteUrl: 'https://sessionize.com/claudiusonel',
     time: '14:00',
-    title: 'Workshop: FinOps in Practice',
-    duration: '120 min',
+    title: 'Workshop: Amazon EKS Autoscaling',
+    duration: '90 min',
   },
 ];
 
